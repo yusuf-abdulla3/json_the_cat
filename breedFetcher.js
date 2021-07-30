9a5b70dab49a57be8d663d663ffa43118ac43477
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-30 00:59:48 +0000

    Refactored the code using callback functions and added a new file called index.js. Also tested the code using Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
+  request("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
+    // console.log('error:', error); // Print the error if one occurred
+   if (error) {
+     callback(error, null);
+   }
+    const data = JSON.parse(body);
+    if (data[0].name.toLowerCase() === breedName.toLowerCase()) {
+      // console.log(data[0].description);
+      callback(null, data[0].description);
+    } else {
+      callback("Sorry, I don't have information on that breed! :(", null);
+    }
+  });
+}; 
+  
 
-request("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  //console.log(typeof body);
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  if (data[0].name.toLowerCase() === breedName.toLowerCase()) {
-    console.log(data[0].description);
-  } else {
-    console.log("Sorry, I don't have information on that breed! :(");
-  }
-});
+module.exports = { fetchBreedDescription };
+
   
